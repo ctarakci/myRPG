87fe536989ccae046ae6d93acceb26f627f69437
Cemal <ctarakci53@gmail.com>
2024-01-03 20:49:15 +0300

    regular update

--- myRPG/main.cpp
+++ myRPG/main.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Vector2f mov(0.0f, 10.0f);
+
+    sf::RenderWindow window(sf::VideoMode(1000, 600), "myRPG");
+    sf::RectangleShape rec(sf::Vector2f(10.0f, 5.0f));
+    rec.setPosition(sf::Vector2f(0.0f, 10.0f));
+    rec.setFillColor(sf::Color::Magenta);
 
     while (window.isOpen())
     {
@@ -14,9 +17,18 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        if(mov.x < 990)
+        {
+            mov.x += 1.0f;
+        }
+        else
+        {
+            mov.y += 1.0f;
+        }
 
+        rec.setPosition(mov);
         window.clear();
-        window.draw(shape);
+        window.draw(rec);
         window.display();
     }
 
